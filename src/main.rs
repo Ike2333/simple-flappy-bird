71320ddd9a70e17c963cdee19cf892706d92a6e6
nooooooooob <ike_233@qq.com>
2025-08-25 12:59:08 +0800

    fix: move initial position

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ enum GameMode {
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0;
+const DEFAULT_PLAYER_X: i32 = 10;
 
 struct State {
     player: Player,
@@ -20,8 +21,6 @@ struct State {
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        // ctx.cls();
-        // ctx.print(1, 1, "Hello, bracket terminal")
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
             GameMode::End => self.dead(ctx),
@@ -33,7 +32,7 @@ impl GameState for State {
 impl State {
     fn new() -> Self {
         State {
-            player: Player::new(5, 25),
+            player: Player::new(DEFAULT_PLAYER_X, 25),
             frame_time: 0.0,
             mode: GameMode::Menu,
             obstacle: Obstacle::new(SCREEN_WIDTH, 0),
@@ -85,23 +84,22 @@ impl State {
         ctx.print(0, 0, "Press space to flap");
         ctx.print(0, 1, &format!("Score: {}", self.score));
         self.obstacle.render(ctx, self.player.x);
-        if self.player.x > self.obstacle.x{
+        if self.player.x > self.obstacle.x {
             self.score += 1;
             self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
         }
 
-        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player){
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
-
     }
 
     fn restart(&mut self) {
-        self.player= Player::new(5, 25);
-        self.frame_time= 0.0;
-        self.mode= GameMode::Playing;
-        self.obstacle= Obstacle::new(SCREEN_WIDTH, 0);
-        self.score= 0;
+        self.player = Player::new(DEFAULT_PLAYER_X, 25);
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.score = 0;
     }
 }
 
@@ -158,20 +156,30 @@ impl Obstacle {
         }
     }
 
-    fn render(&mut self, ctx: &mut BTerm, player_x: i32){
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
         let screen_x = self.x - player_x;
-        let half_size = self.size/2;
-        for y in 0..self.gap_y - half_size{
+        let half_size = self.size / 2;
+        for y in 0..self.gap_y - half_size {
             ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
-        for y in self.gap_y + half_size..SCREEN_HEIGHT{
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
             ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
     }
 
-    fn hit_obstacle(&self, player: &Player) -> bool{
-        let half_size = self.size/2;
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+
+        // println!("player position: ({}, {})", player.x, player.y);
+        // println!("obstacle position x: ({})", self.x);
+
         let does_x_match = player.x == self.x;
+
+        // if does_x_match {
+        //     println!("x coincides: {}", player.x);
+        //     return true;
+        // }
+
         let player_above_gap = player.y < self.gap_y - half_size;
         let player_blow_gap = player.y > self.gap_y + half_size;
         does_x_match && (player_above_gap || player_blow_gap)
@@ -180,7 +188,7 @@ impl Obstacle {
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Flippy Bird")
+        .with_title("Simple Flappy Bird")
         .build()?;
     main_loop(context, State::new())
 }
